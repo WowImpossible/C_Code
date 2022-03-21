aae74351637b2986a292961afd64d696b4720cb7
PeterLiuCode <2473181761@qq.com>
2022-03-21 23:43:20 +0800

    冒泡排序

---
+++ test_3_21/test_3_21.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//冒泡排序，从小到大排序
+int main()
+{
+	int arr[10] = { 0 };
+	int i = 0;
+	int j = 0;
+	int temp = 0;
+	printf("请输入10个数字：\n");
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	printf("\n");
+
+	for (i = 9; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	for (i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+	return 0;
+}
