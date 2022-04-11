3c46a4ea4abd607466146eaa3928aa48afebfb08
PeterLiuCode <2473181761@qq.com>
2022-04-11 21:00:15 +0800

    watch

--- test_3_21/test_3_21.cpp
+++ test_3_21/test_3_21.cpp
@@ -3,32 +3,39 @@
 #include <stdio.h>
 
 //冒泡排序，从小到大排序
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	int temp = 0;
+//	printf("请输入10个数字：\n");
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	printf("\n");
+//
+//	for (i = 9; i > 0; i--)
+//	{
+//		for (j = 0; j < i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//	}
+//	for (i = 0; i < 10; i++)
+//		printf("%d ", arr[i]);
+//	return 0;
+//}
+
 int main()
 {
-	int arr[10] = { 0 };
-	int i = 0;
-	int j = 0;
-	int temp = 0;
-	printf("请输入10个数字：\n");
-	for (i = 0; i < 10; i++)
-	{
-		scanf("%d", &arr[i]);
-	}
-	printf("\n");
-
-	for (i = 9; i > 0; i--)
-	{
-		for (j = 0; j < i; j++)
-		{
-			if (arr[j] > arr[j + 1])
-			{
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-	}
-	for (i = 0; i < 10; i++)
-		printf("%d ", arr[i]);
+	char c[] = { '2','1','0','\0','1','2','\0' };
+	printf("%s", c + 4);
 	return 0;
 }
